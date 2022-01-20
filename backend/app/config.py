eb91c31125df29d22a17d65d0fe396796e175438
Max <s724959099@yahoo.com.tw>
2022-01-20 16:18:53 +0800

    feat: first init

---
+++ backend/app/config.py
@@ -0,0 +1,11 @@
+import os
+from starlette.config import Config
+
+app_path = os.path.dirname(os.path.realpath(__file__))
+env = os.environ.get('ENV')
+env_file_mapping = {
+    'dev': os.path.join(app_path, '.env'),
+    'prod': os.path.join(app_path, '.env.prod'),
+}
+env_path = os.path.join(app_path, env_file_mapping.get(env, '.env'))
+config = Config(env_path)
