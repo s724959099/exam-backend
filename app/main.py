eee5957e6ece608c978c11dfcb0277643d53553a
Max <s724959099@yahoo.com.tw>
2022-01-21 17:57:04 +0800

    fix: main for pylint checking

--- app/main.py
+++ app/main.py
@@ -8,44 +8,51 @@ from config import DEBUG
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
+from utils.log import setup_logging
 
-version = os.environ.get("TAG", "0.0.1")
-app = FastAPI(title="AVL-Exam", version=version)
+version = os.environ.get('TAG', '0.0.1')
+app = FastAPI(title='AVL-Exam', version=version)
 app.debug = DEBUG
 origins = [
-    "*",
+    '*',
 ]
 
-secret_key = "C~u#C$P0T7sdfsdNgnn!vdddS!D2NW{sdfsfG!3"
+secret_key = 'C~u#C$P0T7sdfsdNgnn!vdddS!D2NW{sdfsfG!3'
 app.add_middleware(SessionMiddleware, secret_key=secret_key)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
 
 
-@app.get("/version/")
+@app.get('/version/')
 def get_version():
+    """
+    get version tag
+    """
     return {
-        "version": version,
+        'version': version,
     }
 
 
-@app.on_event("startup")
+@app.on_event('startup')
 async def startup():
-    pass
+    """
+    intitial log before start server
+    """
+    setup_logging()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     if DEBUG:
         uvicorn.run(
-            "main:app",
-            host="0.0.0.0",
+            'main:app',
+            host='0.0.0.0',
             port=5000,
-            log_level="info",
+            log_level='info',
             reload=True,
             workers=1,
             debug=True,
