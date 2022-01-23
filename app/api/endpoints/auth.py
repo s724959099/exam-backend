083ca8af63e23b9a0c27a1c33f812b897f264df0
meatball0520 <meatball0520@gmail.com>
2022-01-24 01:09:22 +0800

    feat: add auth base

--- app/api/endpoints/auth.py
+++ app/api/endpoints/auth.py
@@ -1,3 +1,16 @@
+"""
+Auth api
+"""
 from api.route_handler import init_router_with_log
 
 router = init_router_with_log()
+
+
+@router.get('/login/google/', name='Google Auth login')
+async def google_login():
+    pass
+
+
+@router.get('/login/google/authorized/', name='Google Oauth authorized redirect')
+async def google_login_authorized():
+    pass
