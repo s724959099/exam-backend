3c19c27c333d54592b3099a3de030e86d00162a4
Max <s724959099@yahoo.com.tw>
2022-02-08 16:36:52 +0800

    feat: add secure cookie

--- app/db/schemas/settings.py
+++ app/db/schemas/settings.py
@@ -9,7 +9,7 @@ class Settings(BaseModel):
     """
     authjwt_secret_key: str = config.get('session_secret_key')
     authjwt_token_location: set = {'cookies'}
-    authjwt_cookie_secure: bool = False
+    authjwt_cookie_secure: bool = True
     authjwt_cookie_csrf_protect: bool = True
     # add expire time for testing
     # authjwt_access_token_expires: int = 3
